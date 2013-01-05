ab61141e221cf16a8290b371ac504ef692fd61ab
Glauber <glauberhelcias@hotmail.com>
2013-01-05 17:57:42 -0300

    refatoracao para orientacao a objetos e criacao da sub-classe Dist para distancias

--- crono.js
+++ crono.js
@@ -1,7 +1,8 @@
 var timers = new Array();
 var timelapsed = 0;
+var UNIT = 1000;
 
-function crono(valor_inicial, nome) {
+var Crono = function(valor_inicial, nome) {
 	this.valor_inicial = valor_inicial;
 	this.nome = nome;
 	this.remtimes = 0;
@@ -9,36 +10,36 @@ function crono(valor_inicial, nome) {
 	this.tv = valor_inicial;
 	this.sub_crono = new Array();
 	this.running = false;
-	this.UNIT = 1000;
+};
 
- 	this.clear = function clear() {
+ 	Crono.prototype.clear = function () {
  		this.running=false;
  		timelapsed = 0;
 		this.timeover();
  	};
 
- 	this.x = function x(new_crono) {
+ 	Crono.prototype.x = function (new_crono) {
 		this.sub_crono = this.sub_crono.concat(new_crono);
 		this.tv = this.total_valor();
 	};
 
-	this.subtr = function subtr() {
+	Crono.prototype.subtr = function () {
 			this.valor--;
 			timelapsed++;
 			this.display(this.valor, this.nome, this.remtimes);
 	};
 
-	this.reset = function reset() {
+	Crono.prototype.reset = function () {
 			this.valor = this.valor_inicial;
-      this.remtimes--;
+			this.remtimes--;
 			timelapsed++;
 			this.display_bip(this.valor, this.nome, this.remtimes);
 	};
 
-	this.total_valor = function total_valor() {
+	Crono.prototype.total_valor = function () {
 		var res = 0;
 		if (this.sub_crono.length == 0) {
-			return this.valor;
+			return valor;
 		} else {
 			for(var m=0; m<this.sub_crono.length; m++) {
 				res = res + this.sub_crono[m].tv;
@@ -47,10 +48,10 @@ function crono(valor_inicial, nome) {
 		}
 	};
 
-	this.total_sub_pre = function total_sub_pre(pre) {
+	Crono.prototype.total_sub_pre = function (pre) {
 		var res = 0;
 		if (this.sub_crono.length == 0) {
-			return this.valor;
+			return valor;
 		} else {
 			for(var r=0; r<pre; r++) {
 				res = res + this.sub_crono[r].tv;
@@ -59,10 +60,10 @@ function crono(valor_inicial, nome) {
 		}
 	};
 
-	this.total_sub_pos = function total_sub_pos(pos) {
+	Crono.prototype.total_sub_pos = function (pos) {
 		var res = 0;
 		if (this.sub_crono.length == 0) {
-			return this.valor;
+			return valor;
 		} else {
 			for(var q=pos+1; q<this.sub_crono.length; q++) {
 				res = res + this.sub_crono[q].tv;
@@ -71,25 +72,25 @@ function crono(valor_inicial, nome) {
 		}
 	};
 
-	this.countdown = function countdown(lasttime) {
+	Crono.prototype.countdown = function (lasttime) {
 		if (lasttime>timelapsed) {
 			if (timelapsed>(lasttime-this.valor_inicial)) {
 				var self = this;
 				for(var p=1; p<this.valor; p++) {
-					timers.push(setTimeout(function() { self.subtr(); }, (lasttime-p-timelapsed)*this.UNIT));
+					timers.push(setTimeout(function() { self.subtr(); }, (lasttime-p-timelapsed)*UNIT));
 				}
 			} else {
 				if (timelapsed==0) this.remtimes++;
 				var self = this;
 				for(var p=1; p<this.valor_inicial; p++) {
-					timers.push(setTimeout(function() { self.subtr(); }, (lasttime-p-timelapsed)*this.UNIT));
+					timers.push(setTimeout(function() { self.subtr(); }, (lasttime-p-timelapsed)*UNIT));
 				}
-				timers.push(setTimeout(function() { self.reset(); }, (lasttime-this.valor_inicial-timelapsed)*this.UNIT));
+				timers.push(setTimeout(function() { self.reset(); }, (lasttime-this.valor_inicial-timelapsed)*UNIT));
 			}			
 		}
 	};
 
-	this.run = function run(times, lag, lag2, delay) {
+	Crono.prototype.run = function (times, lag, lag2, delay) {
 		if (this.sub_crono.length == 0) {
 			for (var k=times; k>0; k--) {
 				this.countdown(lag+lag2*k+this.valor_inicial*k+delay*(k-1));
@@ -103,19 +104,26 @@ function crono(valor_inicial, nome) {
 		}
 	};
 	
-	this.abort = function abort() {
+	Crono.prototype.abort = function () {
 		for(this.clear();timers.length>0;clearTimeout(timers.pop()));
 	};
 	
-	this.pause = function pause() {
+	Crono.prototype.pause = function () {
 		this.running = false;
 		for(;timers.length>0;clearTimeout(timers.pop()));
 	};
 
-	this.play = function play() {
+	Crono.prototype.play = function () {
 		this.run(1,0,0,0);
 		this.running = true;
 		var self = this;
-		timers.push(setTimeout(function () { self.clear(); }, (this.tv-timelapsed)*this.UNIT));
+		timers.push(setTimeout(function () { self.clear(); }, (this.tv-timelapsed)*UNIT));
 	};
-}
+
+var Dist = function(valor_inicial, nome) {
+	Crono.call(this, valor_inicial, nome);
+};
+
+	Dist.prototype.countdown = function() {
+		console.log("há de se implementar a contagem regressiva de distâncias");
+	};
--- script.js
+++ script.js
@@ -1,47 +1,52 @@
 /**
  * 
- * Gramática ABNF (http://tools.ietf.org/html/rfc5234) que define uma expressão válida:
+ * Gram�tica ABNF (http://tools.ietf.org/html/rfc5234) que define uma expressão válida:
  * script =  expr *("," [SP] expr)                ; uma expressao ou uma sequência delas
  * expr   =  [1*DIGIT %x27] 1*DIGIT DQUOTE *ALPHA ; minutos e segundos ou só segundos...
  * expr   =/ 1*DIGIT %x27 *ALPHA                  ; ...ou só minutos
  * expr   =/ 1*DIGIT "(" script ")"               ; ou uma repetição sobre um script
  *
  */
-var SCRIPTRULE = /((((\d+\')?\d+\")|(\d+\'))\w*)|(\d+\(.+\))/g;
-var IS_GRP = /\d+\(.+\)/;
-var GRP_CONTENT = /\(.+(?=\))/;
-var MIN = /\d+(?=\')/;
-var SEC = /\d+(?=\")/;
 
-function leiaTokens (expressao) {
-	var tokenlist = expressao.match(SCRIPTRULE);
-	for (var token in tokenlist) {
-		if (IS_GRP.test(tokenlist[token])) {
-			var multiplicador = new crono(tokenlist[token].match(/^\d+/)[0]);
-			conteudo = tokenlist[token].match(GRP_CONTENT)[0].slice(1);
-			multiplicador.x(leiaTokens(conteudo));
-			tokenlist[token] = multiplicador;
-		} else {
-			tempo = 0;
-			if (MIN.test(tokenlist[token])) {
-				tempo += parseInt(tokenlist[token].match(MIN)[0])*60;
-			}
-			if (SEC.test(tokenlist[token])) {
-				tempo += parseInt(tokenlist[token].match(SEC)[0]);
-			}
-			nome = tokenlist[token].match(/\w*$/)[0];
-			tokenlist[token] = new crono(tempo, nome);
+var Script = function(expr) {
+	var SCRIPTRULE = /((((\d+\')?\d+\")|(\d+\'))\w*)|(\d+\(.+\))/g;
+	var IS_GRP = /\d+\(.+\)/;
+	var GRP_CONTENT = /\(.+(?=\))/;
+	var MIN = /\d+(?=\')/;
+	var SEC = /\d+(?=\")/;
+	var cronoarray = readcronoarray(expr);
+	
+	function cronoobj(){
+		if (cronoarray.length > 1) {
+			var newcrono = new crono(1);
+			newcrono.x(cronoarray);
+			return newcrono;
 		}
-	}
-	return tokenlist;
-}
+		return cronoarray[0];
+	};
+	
+	function readcronoarray(expr) {
+		var tokenlist = expr.match(SCRIPTRULE);
+		for (var token in tokenlist) {
+			if (IS_GRP.test(tokenlist[token])) {
+				var multiplicador = new Crono(tokenlist[token].match(/^\d+/)[0]);
+				conteudo = tokenlist[token].match(GRP_CONTENT)[0].slice(1);
+				multiplicador.x(leiaTokens(conteudo));
+				tokenlist[token] = multiplicador;
+			} else {
+				tempo = 0;
+				if (MIN.test(tokenlist[token])) {
+					tempo += parseInt(tokenlist[token].match(MIN)[0])*60;
+				}
+				if (SEC.test(tokenlist[token])) {
+					tempo += parseInt(tokenlist[token].match(SEC)[0]);
+				}
+				nome = tokenlist[token].match(/\w*$/)[0];
+				tokenlist[token] = new Crono(tempo, nome);
+			}
+		}
+		return tokenlist;
+	};
 
-function script (expr) {
-	var cronoarray = leiaTokens(expr);
-	if (cronoarray.length > 1) {
-		var newcrono = new crono(1);
-		newcrono.x(cronoarray);
-		return newcrono;
-	}
-	return cronoarray[0];
-}
+	return cronoobj();
+};
