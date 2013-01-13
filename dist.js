71966cf11b0378f003e43123b9119261786e8c59
Glauber <glauberhelcias@hotmail.com>
2013-01-12 23:05:15 -0300

    durante a contagem de distancia o botao pause funciona, mas no play dá reset; abort ainda não funciona

--- dist.js
+++ dist.js
@@ -10,25 +10,28 @@ var Dist = function(valor_distancia, nome) {
 	Dist.prototype.subtr = function (lastpoint) {
 		if (arguments.length==0) { 
 			this.pause();
+			this.running(true);
 		}
-		//mock obter coordenadas do ponto atual
-		actualpoint = {
-				latitude: -44.25231,
-				longitude: -3.68715
-		};
-		//mockEnd
-		if (lastpoint) {
-			this.valor_distancia -= 3; //distance(lastpoint, actualpoint);
-		}
-		if (this.valor_distancia>0) {
-			var self = this;
-			timers.push(setTimeout(function () { self.subtr(actualpoint); }, this.check_interval*UNIT));
-			this.display(this.valor_distancia, this.nome, this.remtimes);
-		} else {
-			this.remtimes--;
-			this.valor_distancia = this.valor_distancia_inicial;
-			timelapsed = parseFloat((timelapsed + 0.01).toFixed(3));
-			Script.prototype.getInstance().play();
+		if (this.running()) {
+			//mock obter coordenadas do ponto atual
+			actualpoint = {
+					latitude: -44.2531,
+					longitude: -3.6815
+			};
+			//mockEnd
+			if (lastpoint) {
+				this.valor_distancia -= 3; //distance(lastpoint, actualpoint);
+			}
+			if (this.valor_distancia>0) {
+				var self = this;
+				timers.push(setTimeout(function () { self.subtr(actualpoint); }, this.check_interval*UNIT));
+				this.display(this.valor_distancia, this.nome, this.remtimes);
+			} else {
+				this.remtimes--;
+				this.valor_distancia = this.valor_distancia_inicial;
+				timelapsed = parseFloat((timelapsed + 0.01).toFixed(3));
+				Script.prototype.getInstance().play();
+			}
 		}
 	};
 	
