2f3dd43448af5ca7a581b2aca1238adcc4d7a370
glauberhelcias <glauberhelcias@hotmail.com>
2013-03-23 20:45:48 -0300

    usando onchange no status

--- control.js
+++ control.js
@@ -22,23 +22,17 @@ var READY             = 5;
 var RUNNING           = 6;
 var PAUSED            = 7;
 var Control = function() {
-	this.observers = new Array();
-	this.notify = function () {
-		for (var i=0;i<this.observers.length;this.observers[i](),i++);
-	};
+	this.onchange = null;
 	this.set = function (param) {
 		if (this.value!=param) {
 			this.value = param;
-			this.notify();
+			this.onchange();
 		}
 	};
 	this.get = function () {
 		return this.value;
 	};
 };
-
-var grp = 0;
-var status = new Control();
 
 var Formulario = function(name, html_element) {
 	this.name = name;
@@ -49,125 +43,125 @@ var Formulario = function(name, html_element) {
 	};
 };
 
+var grp = 0;
+var status = new Control();
 var frmEdit = new Formulario("frmEdit", "editFrm");
-
-var setbtn = document.createElement("a");
-	setbtn.onclick = function () {
-		switch (status.get())	
-		{
-		case CHOICE:
-		case EDIT_STEP_DIST:
-			status.set(EDIT_STEP_TIME);
-			break;
-		case EDIT_STEP_TIME:
-			status.set(EDIT_STEP_DIST);
-			break;
-		case EDIT_SERIES:
-			//insere autopause no script;
-			break;
-		case PAUSED:
-			//reseta o script
-		case UNFINISHED_SERIES:
-		case READY:
-			status.set(CHOICE);
-			break;
-		case RUNNING:
-			status.set(READY);
-			break;
-		}
-	};
-
 var ctrlbtn = document.createElement("a");
-	ctrlbtn.onclick = function () {
-		switch (status.get())	
-		{
-		case CHOICE:
-			status.set(EDIT_SERIES);
-			break;
-		case EDIT_STEP_TIME:
-		case EDIT_SERIES:
-		case EDIT_STEP_DIST:
-			if (emptyscript()) {
-				status.set(CHOICE);
-			} else {
-				//formulario.incluir_token();
-				if (grp==0) {
-					status.set(READY);
-				} else {
-					status.set(UNFINISHED_SERIES);
-				}
-			}
-			break;
-		case UNFINISHED_SERIES:
-			grp--;
-			//script.fechar_serie;
+var setbtn = document.createElement("a");
+
+setbtn.onclick = function () {
+	switch (status.get())	
+	{
+	case CHOICE:
+	case EDIT_STEP_DIST:
+		status.set(EDIT_STEP_TIME);
+		break;
+	case EDIT_STEP_TIME:
+		status.set(EDIT_STEP_DIST);
+		break;
+	case EDIT_SERIES:
+		//insere autopause no script;
+		break;
+	case PAUSED:
+		//reseta o script
+	case UNFINISHED_SERIES:
+	case READY:
+		status.set(CHOICE);
+		break;
+	case RUNNING:
+		status.set(READY);
+		break;
+	}
+};
+
+ctrlbtn.onclick = function () {
+	switch (status.get())	
+	{
+	case CHOICE:
+		status.set(EDIT_SERIES);
+		break;
+	case EDIT_STEP_TIME:
+	case EDIT_SERIES:
+	case EDIT_STEP_DIST:
+		if (emptyscript()) {
+			status.set(CHOICE);
+		} else {
+			//formulario.incluir_token();
 			if (grp==0) {
 				status.set(READY);
 			} else {
 				status.set(UNFINISHED_SERIES);
 			}
-			break;
-		case READY:
-		case PAUSED:
-			status.set(RUNNING);
-			break;
-		case RUNNING:
-			status.set(PAUSED);
-			break;
 		}
-	};
-
-status.observers.push( 
-	function () {
-		frmEdit.statichtml = "";
-		switch (status.get())
-		{
-		case CHOICE:
-			ctrlbtn.textContent = "Series";
-			setbtn.textContent = "Step";
-			break;
-		case EDIT_STEP_TIME:
-			ctrlbtn.textContent = "Ok";
-			setbtn.textContent = "Dist";
-			frmEdit.statichtml  = FRMSTEPHTML;
-			break;
-		case EDIT_SERIES:
-			ctrlbtn.textContent = "Ok";
-			setbtn.textContent = "AutoPause";
-			frmEdit.statichtml = FRMSERIESHTML;
-			break;
-		case EDIT_STEP_DIST:
-			ctrlbtn.textContent = "Ok";
-			setbtn.textContent = "Time";
-			frmEdit.statichtml = FRMDISTHTML;
-			break;
-		case UNFINISHED_SERIES:
-			ctrlbtn.textContent = ")";
-			setbtn.textContent = "Add";
-			break;
-		case READY:
-			ctrlbtn.textContent = "Play";
-			setbtn.textContent = "Add";
-			break;
-		case PAUSED:
-			ctrlbtn.textContent = "Play";
-			setbtn.textContent = "Reset";
-			break;
-		case RUNNING:
-			ctrlbtn.textContent = "Pause";
-			setbtn.textContent = "Abort";
-			break;
+		break;
+	case UNFINISHED_SERIES:
+		grp--;
+		//script.fechar_serie;
+		if (grp==0) {
+			status.set(READY);
+		} else {
+			status.set(UNFINISHED_SERIES);
 		}
-		frmEdit.display();
+		break;
+	case READY:
+	case PAUSED:
+		status.set(RUNNING);
+		break;
+	case RUNNING:
+		status.set(PAUSED);
+		break;
 	}
-);
+};
+
+status.onchange = function () {
+	frmEdit.statichtml = "";
+	switch (status.get())
+	{
+	case CHOICE:
+		ctrlbtn.textContent = "Series";
+		setbtn.textContent = "Step";
+		break;
+	case EDIT_STEP_TIME:
+		ctrlbtn.textContent = "Ok";
+		setbtn.textContent = "Dist";
+		frmEdit.statichtml  = FRMSTEPHTML;
+		break;
+	case EDIT_SERIES:
+		ctrlbtn.textContent = "Ok";
+		setbtn.textContent = "AutoPause";
+		frmEdit.statichtml = FRMSERIESHTML;
+		break;
+	case EDIT_STEP_DIST:
+		ctrlbtn.textContent = "Ok";
+		setbtn.textContent = "Time";
+		frmEdit.statichtml = FRMDISTHTML;
+		break;
+	case UNFINISHED_SERIES:
+		ctrlbtn.textContent = ")";
+		setbtn.textContent = "Add";
+		break;
+	case READY:
+		ctrlbtn.textContent = "Play";
+		setbtn.textContent = "Add";
+		break;
+	case PAUSED:
+		ctrlbtn.textContent = "Play";
+		setbtn.textContent = "Reset";
+		break;
+	case RUNNING:
+		ctrlbtn.textContent = "Pause";
+		setbtn.textContent = "Abort";
+		break;
+	}
+	frmEdit.display();
+};
 
 function emptyscript () {
 	return false;
 };
 
 function init() {
-	document.getElementById("secndBtn").appendChild(ctrlbtn);
 	document.getElementById("firstBtn").appendChild(setbtn);
+	document.getElementById("secndBtn").appendChild(ctrlbtn);
 	status.set(CHOICE);
-}
+};
